3ae4bc80275e8033c18f32e1dcaad4511fd9ea59
SHDGMSQ <sh.dg.msq@mail.ru>
2022-11-17 20:49:33 +0300

    creating todolists with tasks first step

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Todolist} from './Todolist';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+
+    const task1 = [
+        {id: 1, title: 'JS', isDone: true},
+        {id: 2, title: 'Node', isDone: false},
+        {id: 3, title: 'Vue', isDone: false},
+    ]
+
+    const task2 = [
+        {id: 1, title: 'Hello', isDone: true},
+        {id: 2, title: 'bla', isDone: false},
+        {id: 3, title: 'teach', isDone: true},
+    ]
+
+
+    return (
+        <div className='App'>
+            <Todolist title='What to learn' tasks={task1}/>
+            <Todolist title='Lets go' tasks={task2}/>
+        </div>
+    );
+};
 
 export default App;
